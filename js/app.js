02fe3cb56392f18d558d3402b8571a305d833a1a
Evannfries <evann.fries@yahoo.com>
2022-07-06 18:17:12 -0500

    tamagotchi class made

---
+++ js/app.js
@@ -0,0 +1,17 @@
+class Tamagotchi {
+    constructor(name){
+        name = this.name
+        this.hunger = 1
+        this.sleepiness = 1
+        this.boredom = 1
+        this.age = 0
+    }
+  
+}
+
+
+const eddie = new Tamagotchi ('Eddie')
+console.log(eddie)
+class Game {
+    
+}
